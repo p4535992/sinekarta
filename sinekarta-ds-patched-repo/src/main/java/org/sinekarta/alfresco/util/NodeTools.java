5e79f1aba19ab865639e01c3a1571c237ce9d2ce
Marco Tenti <marco.tenti@abd.it>
2019-06-25 13:41:29 +0200

    [BETA] Update

--- sinekarta-ds-patched-repo/src/main/java/org/sinekarta/alfresco/util/NodeTools.java
+++ sinekarta-ds-patched-repo/src/main/java/org/sinekarta/alfresco/util/NodeTools.java
@@ -14,19 +14,15 @@
  * GNU General Public License for more details.
  *
  */
-package org.sinekartads.alfresco.util;
+package org.sinekarta.alfresco.util;
 
-import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.search.impl.lucene.QueryParameterisationException;
 import org.alfresco.service.cmr.model.FileExistsException;
 import org.alfresco.service.cmr.model.FileFolderService;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
-import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.Path;
@@ -36,9 +32,7 @@ import org.alfresco.service.cmr.search.ResultSet;
 import org.alfresco.service.cmr.search.SearchService;
 import org.alfresco.service.cmr.security.OwnableService;
 import org.alfresco.service.namespace.NamespaceService;
-import org.alfresco.service.namespace.QName;
 import org.alfresco.util.ISO9075;
-import org.apache.commons.collections.MapUtils;
 import org.apache.log4j.Logger;
 import org.sinekarta.alfresco.exception.InvalidPathException;
 import org.sinekarta.alfresco.exception.SignFailedException;
@@ -54,78 +48,6 @@ public class NodeTools {
 	
 	private static Logger tracer = Logger.getLogger(NodeTools.class);
 	
-	/**
-	 * verify that a filename or folder name contains only valid character
-	 * @param name
-	 * @return
-	 */
-	public static String validateFileFolderName(String name) {
-		if (name!=null) {
-			name = name.replaceAll("~", "-");
-			name = name.replaceAll("&", "-");
-			name = name.replaceAll("#", "-");
-			name = name.replaceAll("\\*", "-");
-			name = name.replaceAll("\\?", "-");
-			name = name.replaceAll(":", "-");
-			
-			name = name.replaceAll("\"", "'");
-			
-			name = name.replaceAll("\\|", "-");
-			name = name.replaceAll("\\\\", "-");
-			name = name.replaceAll("/", "-");
-			
-			name = name.replaceAll("<", "(");
-			name = name.replaceAll(">", ")");
-			name = name.replaceAll("\\{", "(");
-			name = name.replaceAll("}", ")");
-		}
-		return name.trim();
-	}
-	
-	/**
-	 * Create a node with the given name and the given properties, as a child of
-	 * a certain parent.
-	 * The properties are forced to contain the given name as PROP_NAME
-	 * @param nodeService the service to be used 
-	 * @param parentRef the parent's NodeRef 
-	 * @param name the 
-	 * @return the NodeRef to the generated child node
-	 * @throws InvalidNodeRefException if the parentRef is invalid or if a child 
-	 * with that name already exists
-	 */
-	public static NodeRef createNode(
-			NodeService nodeService, NodeRef parentRef, String name)  
-					throws InvalidNodeRefException {
-		return createNode(nodeService, parentRef, name, null);
-	}	
-	
-	/**
-	 * Create a node with the given name and the given properties, as a child of
-	 * a certain parent.
-	 * The properties are forced to contain the given name as PROP_NAME
-	 * @param nodeService the service to be used 
-	 * @param parentRef the parent's NodeRef 
-	 * @param name the name to assign to the child node
-	 * @param properties the properties to be assigned to the child node 
-	 * @return the NodeRef to the generated child node
-	 * @throws InvalidNodeRefException if the parentRef is invalid or if a child 
-	 * with that name already exists
-	 */
-	public static NodeRef createNode(
-			NodeService nodeService, NodeRef parentRef, String name, Map<QName, Serializable> properties)  
-					throws InvalidNodeRefException {
-		
-		if(MapUtils.isEmpty(properties)) {
-			properties = new HashMap<QName, Serializable>();
-		}
-		properties.put(ContentModel.PROP_NAME, name);
-		return nodeService.createNode ( parentRef, 
-										ContentModel.ASSOC_CONTAINS, 
-										QName.createQName(NamespaceService.CONTENT_MODEL_PREFIX, name),	            
-										ContentModel.TYPE_CONTENT, 
-										properties ).getChildRef();
-	}
-
 	/**
 	 * convert a path into a lucene compliant String
 	 * all namespace are converted to corresponding prefix
@@ -166,45 +88,6 @@ public class NodeTools {
 		}
 		if (tracer.isDebugEnabled()) tracer.debug("translatePath : " + path + " = " + ret);
 		return ret.toString();
-	}
-	
-	/**
-	 * convert a path into a human readable String
-	 * 
-	 * @param nodeService used to ask to each node his readable name
-	 * @param path the path to translate
-	 * @return a human readable string representation of the path
-	 */
-	public static String translatePath(NodeService nodeService, NodeRef nodeRef) {
-		Path path = nodeService.getPath(nodeRef);
-		StringBuffer ret = new StringBuffer();
-		Iterator<Path.Element> iter = path.iterator();
-		iter.next(); // skipping first "/" path
-		while (iter.hasNext()) {
-			ret.append("/");
-			Path.Element el = iter.next();
-			ChildAssociationRef elementRef = ((ChildAssocElement)el).getRef();
-			ret.append(nodeService.getProperty(elementRef.getChildRef(), ContentModel.PROP_NAME));
-		}
-		QName nodeType = nodeService.getType(nodeRef);
-		if(nodeType.isMatch(ContentModel.TYPE_FOLDER)) {
-			ret.append("/");
-		}
-		if (tracer.isDebugEnabled()) tracer.debug("translatePath : " + path + " = " + ret);
-		return ret.toString();
-	}
-	
-	public static NodeRef getParentRef(NodeService nodeService, NodeRef nodeRef) {
-		Iterator<ChildAssociationRef> it = nodeService.getParentAssocs(nodeRef).iterator();
-		NodeRef parentRef = null;
-		ChildAssociationRef childAssoc;
-		while (it.hasNext() && parentRef == null) {
-			childAssoc = it.next();
-			if (childAssoc.getTypeQName().equals(ContentModel.ASSOC_CONTAINS)) {
-				parentRef = childAssoc.getParentRef();
-			}
-		}
-		return parentRef;
 	}
 
 	/**
@@ -273,34 +156,6 @@ public class NodeTools {
 	@SuppressWarnings("static-access")
 	public static NodeRef deepCreateFolder(NodeService nodeService, SearchService searchService, FileFolderService fileFolderService, StoreRef storeRef, String lucenePath) {
 		if (tracer.isDebugEnabled()) tracer.debug("deepCreateFolder, path to create : " + lucenePath);
-		// scompongo il path da cercare
-		String[] pp = lucenePath.split("/");
-		String path="";
-		int idxStart;
-		for (idxStart=1;idxStart<pp.length;idxStart++) {
-			if (pp[idxStart].startsWith("app"))
-				path=path+"/"+pp[idxStart];
-			else 
-				break;
-		}
-		// get node of path requested
-		NodeRef parent = getNodeRefByPath(searchService, nodeService, storeRef, path);
-		NodeRef folder = parent; 
-		for (int i=idxStart;i<pp.length;i++) {
-			String folderName = pp[i].split(":")[1];
-			folderName = validateFileFolderName(folderName);
-			parent=folder;
-			folder = fileFolderService.searchSimple(parent,folderName);
-			if (folder==null)
-				folder = fileFolderService.create(parent, folderName,ContentModel.TYPE_FOLDER).getNodeRef();
-		}
-		if (tracer.isDebugEnabled()) tracer.debug("deepCreateFolder, path " + lucenePath + " created.");
-		return folder;
-	}
-
-	/*
-	public static NodeRef deepCreateFolder(NodeService nodeService, SearchService searchService, FileFolderService fileFolderService, StoreRef storeRef, String lucenePath) {
-		if (tracer.isDebugEnabled()) tracer.debug("deepCreateFolder, path to create : " + lucenePath);
 		// get node of path requested
 		NodeRef node = getNodeRefByPath(searchService, nodeService, storeRef, lucenePath);
 		// if exist, ok, return it
@@ -331,7 +186,6 @@ public class NodeTools {
 		if (tracer.isDebugEnabled()) tracer.debug("deepCreateFolder, path " + lucenePath + " created.");
 		return ret;
 	}
-	*/
 
 	/**
 	 * 	calculate the NodeRef of a giving lucenePath (the path should return only one result)
